e7f0a55cc84e958ccf1612279d4f67d6782e87fb
SenhorJedi <daniel_aalegria@hotmail.com>
2022-01-28 15:24:59 -0400

    Update artigo.dart
    atributos setados

---
+++ wikidoscorno/lib/model/artigo.dart
@@ -0,0 +1,8 @@
+class Artigo {
+  Artigo(this.uuid, this.titulo, this.conteudo, this.linguagem, this.tag);
+  final String uuid;
+  final String titulo;
+  final String conteudo;
+  final String linguagem;
+  final String tag;
+}
